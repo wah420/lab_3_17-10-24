72be6a79020974bd55adf94e43fefc1756a3b3f4
Cai Perkins (s5623774) <s5623774@bournemouth.ac.uk>
2024-10-17 17:41:56 +0100

    Lab 3

---
+++ functions.py
@@ -0,0 +1,2 @@
+def new_function(x):
+    return(x)
---
+++ reverse_list.py
@@ -0,0 +1,24 @@
+
+def reverse_list2(my_list):
+    """
+    Reverse the values in a list containing exactly two numbers.
+
+    Input:
+    lst (list): A list containing exactly two numbers.
+
+    Returns:
+    list: The list with its two values swapped.
+    
+    Example:
+    --------
+    >>> reverse_list([5, 10])
+    [10, 5]
+
+    """
+   
+    # Reverse the values in the list
+    # Complete your code here...
+
+list = [98, 12]
+print(reverse_list)
+#return my_list
